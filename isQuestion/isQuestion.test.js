8452b94968a2555ffb0c985fde3cfa2a113d5553
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-09 13:11:27 +0100

    Test case for the isQuestion function passed, reinforced.

--- isQuestion/isQuestion.test.js
+++ isQuestion/isQuestion.test.js
@@ -4,4 +4,7 @@ describe('isQuestion function returns true is the string ends with a question ma
     it('Case 1: Testing ""qwerty?" to see if it\'s a question',()=>{
         expect("qwerty?".isQuestion()).toBeTruthy();
     });
+    it('Case 1: Testing ""qwerty?" to see if it\'s a question',()=>{
+        expect("Is this a real question?".isQuestion()).toBeTruthy();
+    });
 });
