4be5f6ccbf68b78ab3291d9dfe107003bc2611b8
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-09 15:07:53 +0100

    Test case for wordCount function initialized, passed.

--- wordCount/wordCount.js
+++ wordCount/wordCount.js
@@ -1,5 +1,6 @@
 require('../words/words');
 
 String.prototype.wordCount = function(){
-    
+    var words = this.words();
+    return words.length;
 }
--- words/words.test.js
+++ words/words.test.js
@@ -4,4 +4,7 @@ describe('words function returns a list of the words in the array as an array',(
     it('Case 1: Test "How many words are here?"',()=>{
         expect('How many words are here'.words()).toEqual(['How','many','words','are','here']);
     });
+    it('Case 2: Test "How many words, are now here; since we\'ve added some more?"',()=>{
+        expect('How many words, are now here; since we\'ve added some more?'.words()).toEqual(['How', 'many', 'words', 'are', 'now', 'here', 'since', 'we', 've', 'added', 'some', 'more']);
+    });
 });
