01c51a2cc0f79c34f55c0ec9c4f9e1384acfcdc0
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-09 01:48:22 +0100

    Test case for the toupper function that returns a  string with all string characters capitalized passed.

--- toUpper/toUpper.js
+++ toUpper/toUpper.js
@@ -1,5 +1,15 @@
-let toUpper = function(){
-
+let toUpper = function(testString){
+    var upperCase = '', asciiCode = 0;
+    let regexTest = /[a-z]/;
+    for(var index = 0; index < testString.length; index++){
+        if(regexTest.test(testString[index]) === true){
+            asciiCode = testString.charCodeAt(index) - 32;
+            upperCase+=String.fromCharCode(asciiCode);
+        }else{
+            upperCase+=testString[index];
+        }
+    }
+    return upperCase;
 }
 
 module.exports = toUpper;
--- toUpper/toUpper.test.js
+++ toUpper/toUpper.test.js
@@ -1,5 +1,6 @@
 const ToUpper = require('./toUpper');
 
+console.log(ToUpper("qwerty"));
 describe('toUpper function returns the string but with all string characters in Upper Case',()=>{
     it('Case 1: changing "qwerty" to upper case',()=>{
         expect(ToUpper("qwerty")).toBe("QWERTY");
