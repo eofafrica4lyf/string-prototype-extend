949132e4fb241f372160a165c29bb905e5abf3c3
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-09 14:20:59 +0100

    Test case for returning the words as an array initialized, passed.

--- words/words.js
+++ words/words.js
@@ -1,3 +1,15 @@
 String.prototype.words = function(){
+    // var regexTest = /(\w)+.*?/;
+    var regexTest = /(\b\w+\b)+/g;
+    var wordMatches = this.match(regexTest);
+    var words = [];
     
+    for(index = 0;;index++){
+        if(wordMatches[index] === undefined){
+            break;
+        }else{
+            words.push(wordMatches[index]);
+        }
+    }
+    return words;
 }
--- words/words.test.js
+++ words/words.test.js
@@ -2,6 +2,6 @@ require('./words');
 
 describe('words function returns a list of the words in the array as an array',()=>{
     it('Case 1: Test "How many words are here?"',()=>{
-        expect("How many words are here".words()).toBe(["How","many","words","are","here"]);
+        expect('How many words are here'.words()).toEqual(['How','many','words','are','here']);
     });
 });
