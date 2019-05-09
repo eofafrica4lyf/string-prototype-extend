2b1299a457d0d9252587b9a72e425b73f11c60d5
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-09 13:22:37 +0100

    Test case for returning the words as an array initialized, failed.

---
+++ words/words.test.js
@@ -0,0 +1,7 @@
+require('./words');
+
+describe('words function returns a list of the words in the array as an array',()=>{
+    it('Case 1: Test "How many words are here?"',()=>{
+        expect("How many words are here".words()).toBe(["How","many","words","are","here"]);
+    });
+});
