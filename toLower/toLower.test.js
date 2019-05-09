f566f557aa9834c6b38c1459bd6e4b823bc660fb
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-09 11:25:49 +0100

    Test case for the tolower function that returns a string with all string characters in lower case  passed, reinforced.

--- toLower/toLower.test.js
+++ toLower/toLower.test.js
@@ -4,4 +4,7 @@ describe('toLower function returns the string but with all string characters in 
     it('Case 1: changing QWERTY to lower case',()=>{
         expect(ToLower("QWERTY")).toBe("qwerty");
     });
+    it('Case 2: changing 123,4/56 to lower case',()=>{
+        expect(ToLower("123,4/56")).toBe("123,4/56");
+    });
 }); 
