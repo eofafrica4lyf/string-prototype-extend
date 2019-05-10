1e966d46ba1ed0806aa4b7754fb5f9557061db7a
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-10 01:41:34 +0100

    Test case for inverseCase function initialized, passed.

--- inverseCase/inverseCase.js
+++ inverseCase/inverseCase.js
@@ -1,3 +1,17 @@
 String.prototype.inverseCase = function () { 
-    
+    var inverseCase = '', asciiCode = 0;
+    var regexTest = /[a-z]/i;
+    for(var index = 0; index < this.length; index++){
+        if(regexTest.test(this[index]) === true && this.charCodeAt(index) >= 97){
+            asciiCode = this.charCodeAt(index) - 32;
+            inverseCase+=String.fromCharCode(asciiCode);
+        }else if(regexTest.test(this[index]) === true && this.charCodeAt(index) < 97){
+            asciiCode = this.charCodeAt(index) + 32;
+            inverseCase+=String.fromCharCode(asciiCode);
+        }else{
+            inverseCase+=this[index];
+        }
+    }
+    return inverseCase;
 }
+console.log('Mr. Ben'.inverseCase());
--- inverseCase/inverseCase.test.js
+++ inverseCase/inverseCase.test.js
@@ -2,6 +2,6 @@ require('./inverseCase');
 
 describe('inverseCase function that eturns each letter in the string as an inverse of its current case ',()=>{
     it('Case 1: "Mr. Ben" returns "mR. bEN"',()=>{
-        expect('Mr. Ben'.toCurrency()).toEqual('mR. bEN');
+        expect('Mr. Ben'.inverseCase()).toEqual('mR. bEN');
     });
 });
